4e3387b90cf2955c9390dcc0536a13ac68a07d31
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-04 19:34:49 +0530

    Updated config

--- EcomBackend/src/main/java/com/example/EcomBackend/config/WebSecurityConfig.java
+++ EcomBackend/src/main/java/com/example/EcomBackend/config/WebSecurityConfig.java
@@ -2,8 +2,13 @@ package com.example.EcomBackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
 
 @Configuration
 public class WebSecurityConfig {
@@ -11,4 +16,17 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // disable CSRF for testing APIs
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()  // allow public access to signup/signin
+                        .anyRequest().authenticated()                // restrict others
+                )
+                .httpBasic(Customizer.withDefaults()); // simple basic auth config
+
+        return http.build();
+    }
+
 }
