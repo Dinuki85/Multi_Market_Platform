800ed794f7202d305509d2f53aea3dbad5822e83
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-04 18:11:38 +0530

    Feat:Added enum type for both role

--- EcomBackend/src/main/java/com/example/EcomBackend/model/User.java
+++ EcomBackend/src/main/java/com/example/EcomBackend/model/User.java
@@ -15,15 +15,23 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    public enum Role{
+        BUYER,
+        SELLER
+    }
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
     public User(){
 
     }
 
-    public User(Long id, String email, String name, String password) {
+    public User(Long id, String email, String name, String password, Role role) {
         this.id = id;
         this.email = email;
         this.name = name;
         this.password = password;
+        this.role = role;
     }
 
     public Long getId() {
@@ -56,5 +64,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 }
