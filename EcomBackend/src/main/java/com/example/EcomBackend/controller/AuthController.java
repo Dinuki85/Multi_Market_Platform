2ed9243d3225eb7351efc52d0f986650c6deda99
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-04 19:22:43 +0530

    Added endpoints to the signup

---
+++ EcomBackend/src/main/java/com/example/EcomBackend/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.example.EcomBackend.controller;
+
+import com.example.EcomBackend.dto.SignupRequest;
+import com.example.EcomBackend.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/auth")
+@RestController
+@CrossOrigin(origins = "*")
+public class AuthController {
+    @Autowired
+    public AuthService authService;
+
+    @PostMapping("/signup")
+   public String signup(@RequestBody SignupRequest request){
+       return authService.register(request);
+   }
+
+
+
+}
