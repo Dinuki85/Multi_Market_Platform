426cbd281c77df4c39965b83ccc80c7319a584ed
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-04 18:55:31 +0530

    Created metho for the sign in

--- EcomBackend/src/main/java/com/example/EcomBackend/service/AuthService.java
+++ EcomBackend/src/main/java/com/example/EcomBackend/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.example.EcomBackend.service;
 
+import com.example.EcomBackend.dto.SigninRequest;
 import com.example.EcomBackend.dto.SignupRequest;
 import com.example.EcomBackend.model.User;
 import com.example.EcomBackend.repository.UserRepository;
@@ -25,6 +26,15 @@ public class AuthService {
         user.setRole(request.role);
         userRepository.save(user);
         return "User registered Successfully as "+ request.role;
+
+    }
+
+    public String login(SigninRequest request){
+        User user = userRepository.findByEmail(request.email).orElse(null);
+        if(user == null || !passwordEncoder.matches(request.password, user.getPassword())){
+            return "Invalid email or password";
+        }
+        return "Login Successfull .Role: "+user.getRole();
 
     }
 
