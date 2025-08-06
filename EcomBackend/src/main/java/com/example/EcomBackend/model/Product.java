f57ba3c8ac57e2be121f60e820a645eaf4557f3c
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-06 16:39:46 +0530

    Feat:Added seller's product management

---
+++ EcomBackend/src/main/java/com/example/EcomBackend/model/Product.java
@@ -0,0 +1,96 @@
+package com.example.EcomBackend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    private Long id ;
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+
+    public enum Category{
+        KITCHEN,
+        HANDMADE,
+        CLOTHES_MENS,
+        CLOTHES_WOMEN,
+        DAILY_ESSENTIALS
+    }
+    @Enumerated(EnumType.STRING)
+    private Category category ;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User seller;
+
+    public Product(){
+
+    }
+
+    public Product(Long id, String name, String description, double price, int stock, Category category, User seller) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+        this.seller = seller;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+}
