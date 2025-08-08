1620642c63b1e33d980623ed225993b88dde1d08
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-08 11:30:51 +0530

    Updated AUthController

--- EcomBackend/src/main/java/com/example/EcomBackend/controller/AuthController.java
+++ EcomBackend/src/main/java/com/example/EcomBackend/controller/AuthController.java
@@ -4,7 +4,10 @@ import com.example.EcomBackend.dto.SigninRequest;
 import com.example.EcomBackend.dto.SignupRequest;
 import com.example.EcomBackend.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequestMapping("/api/auth")
 @RestController
@@ -14,9 +17,14 @@ public class AuthController {
     public AuthService authService;
 
     @PostMapping("/signup")
-   public String signup(@RequestBody SignupRequest request){
-       return authService.register(request);
-   }
+   public ResponseEntity<Map<String,Object>> signup(@RequestBody SignupRequest request){
+        String result = authService.register(request);
+        boolean success = !result.toLowerCase().contains("already");
+        return  ResponseEntity.status(success ? 200 : 400).body(Map.of("success",success,"message",result));
+    }
+
+
+
     @PostMapping("/signin")
     public String signup(@RequestBody SigninRequest request){
         return authService.login(request);
